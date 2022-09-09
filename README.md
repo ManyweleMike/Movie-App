860e0aa502c32a1d330d36b80b2aca79ade2806d
ManyweleMike <mugendimike80@gmail.com>
2022-09-09 17:36:35 +0300

    changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Movie-App
+
+<h3>MovieWEb</h3>
+<p> A web app to eneble users search their favourite movie content.</p>
+
--- script.js
+++ script.js
@@ -68,13 +68,13 @@ function displayMovieDetails(details){
     <div class = "movie-info">
         <h3 class = "movie-title">${details.Title}</h3>
         <ul class = "movie-misc-info">
-            <li class = "year">Year: ${details.Year}</li>
+            <li class = "year">Year:  ${details.Year}</li>
             <li class = "released">Released: ${details.Released}</li>
         </ul>
         <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
         <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
         <p class = "actors"><b>Actors: </b>${details.Actors}</p>
-        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
 
     </div>
     <div class = "movie-poster">
